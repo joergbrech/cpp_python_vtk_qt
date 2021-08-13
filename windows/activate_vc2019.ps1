b3af0d720cbec399d3c63bdd3971800d0e8cda31
Jan Kleinert <jan.kleinert@dlr.de>
2021-08-13 10:57:55 +0200

    add activate_vc2019.ps1

---
+++ windows/activate_vc2019.ps1
@@ -0,0 +1,9 @@
+# loads in the visual c++ 2019 compiler chain
+
+cmd.exe /c "call `"C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat`" && set > %temp%\vcvars.txt"
+
+Get-Content "$env:temp\vcvars.txt" | Foreach-Object {
+  if ($_ -match "^(.*?)=(.*)$") {
+	Set-Content "env:\$($matches[1])" $matches[2]
+  }
+}
